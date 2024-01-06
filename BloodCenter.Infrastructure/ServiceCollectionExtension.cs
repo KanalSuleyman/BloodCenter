cd23137321df40d71f735ed6a86acb27b7a2710b
suleymanKanal <suleyman-kanal@hotmail.com>
2024-01-07 00:21:17 +0300

    BloodTestRepository and RoleRepository classes registered as a service using the AddTransient method.

---
+++ BloodCenter.Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,21 @@
+﻿using BloodCenter.Application.Repositories.Abstract;
+using BloodCenter.Infrastructure.Repositories.Concrete;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Infrastructure
+{
+    public static class ServiceCollectionExtension
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            services.AddTransient<IBloodTestRepository, BloodTestRepository>();
+            services.AddTransient<IRoleRepository, RoleRepository>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+        }
+    }
+}
