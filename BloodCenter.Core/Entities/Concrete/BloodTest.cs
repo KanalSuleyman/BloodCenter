5fb97d1fbeb0e77bdac5e464e8754fb70739f2e6
suleymanKanal <suleyman-kanal@hotmail.com>
2024-01-07 00:14:40 +0300

    BloodTest and Role entities created

---
+++ BloodCenter.Core/Entities/Concrete/BloodTest.cs
@@ -0,0 +1,13 @@
+﻿using BloodCenter.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Core.Entities.Concrete
+{
+    public class BloodTest : EntityBase, IEntity
+    {
+    }
+}
---
+++ BloodCenter.Core/Entities/Concrete/Role.cs
@@ -0,0 +1,14 @@
+﻿using BloodCenter.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Core.Entities.Concrete
+{
+    public class Role : EntityBase, IEntity
+    {
+
+    }
+}
