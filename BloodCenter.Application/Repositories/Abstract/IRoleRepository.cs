2d0e5d8280be358cc16aa0497b8a392e676f633f
suleymanKanal <suleyman-kanal@hotmail.com>
2024-01-07 00:17:19 +0300

    Interfaces for BloodTest and Role repositories created and used in UnitOfWork pattern

---
+++ BloodCenter.Application/Repositories/Abstract/IRoleRepository.cs
@@ -0,0 +1,14 @@
+﻿using BloodCenter.Core.Entities.Concrete;
+using BloodCenter.Shared.Application.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Application.Repositories.Abstract
+{
+    public interface IRoleRepository : IEntityRepository<Role>
+    {
+    }
+}
---
+++ BloodCenter.Application/Repositories/Abstract/IUnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Application.Repositories.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IBloodTestRepository BloodTests { get; }
+        IRoleRepository Roles { get; }
+
+        void BeginTransaction();
+        void CommitTransaction();
+        void RollbackTransaction();
+    }
+}
