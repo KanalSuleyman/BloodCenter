07c464f654c92b788fa5533098ba45b85a0f58f5
suleymanKanal <suleyman-kanal@hotmail.com>
2024-01-07 00:12:29 +0300

    Repository base methods added as a generic interface for the application layer.

---
+++ BloodCenter.Shared/Application/Abstract/IEntityRepository.cs
@@ -0,0 +1,19 @@
+﻿using BloodCenter.Shared.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BloodCenter.Shared.Application.Abstract
+{
+    public interface IEntityRepository<T> where T : class, IEntity, new()
+    {
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task<string> AddAsync(T entity);
+        Task<string> UpdateAsync(T entity);
+        Task<string> DeleteAsync(int id);
+    }
+}
